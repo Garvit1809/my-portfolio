49a17e353d658a15c8c9f6049c226c6e5cdae077
Garvit1809 <89806031+Garvit1809@users.noreply.github.com>
2022-05-27 00:08:29 +0530

    Hehehehe

--- src/App.js
+++ src/App.js
@@ -1,24 +1,42 @@
-import logo from './logo.svg';
-import './App.css';
+// import { Route, Switch } from "react-router-dom"
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+// import { Switch } from "react-router-dom/cjs/react-router-dom.min"
+import { ThemeProvider } from "styled-components";
+import { lightTheme } from "./components/Themes";
+import GlobalStyle from "./GlobalStyles";
+
+// import components here
+import Main from "./components/Main";
+import AboutPage from "./components/AboutPage";
+import BlogPage from "./components/BlogPage";
+import WorkPage from "./components/WorkPage";
+import MySkillPage from "./components/MySkillpage";
+import SoundBar from "./subComponents/SoundBar";
+
+// import { useLocation } from "react-router-dom";
+import { AnimatePresence } from "framer-motion";
 
 function App() {
+  // const location = useLocation();
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <GlobalStyle />
+
+      <ThemeProvider theme={lightTheme}>
+        <SoundBar />
+
+        <AnimatePresence exitBeforeEnter>
+      
+            <Routes>
+              <Route exact path="/" element={<Main />} />
+              <Route exact path="/about" element={<AboutPage />} />
+              <Route exact path="/blog" element={<BlogPage />} />
+              <Route exact path="/work" element={<WorkPage />} />
+              <Route exact path="/skills" element={<MySkillPage />} />
+            </Routes>
+        </AnimatePresence>
+      </ThemeProvider>
+    </>
   );
 }
 
---
+++ src/data/WorkData.js
@@ -0,0 +1,80 @@
+export const Work = [
+    {
+        id:1,
+        name:"Taxi Animation",
+        description: "A simple moving car animation using simple css",
+        tags:["html","css"],
+
+        demo:"garvit1809.github.io/Taxi-Animation/",
+        github:"github.com/Garvit1809/Taxi-Animation/tree/master"
+    },
+    {
+        id:2,
+        name:"Drum-Kit",
+        description: "A simple drum-kit website built using javascipt dom and event listeners only",
+        tags:["html","css","javascript"],
+
+        demo:"garvit1809.github.io/Drum-Kit",
+        github:"github.com/Garvit1809/Drum-Kit"
+    },
+    {
+        id:3,
+        name:"Form-Validation",
+        description: "Adding form validation on the client side using javascript",
+        tags:["html","css","javascript"],
+
+        demo:"garvit1809.github.io/Form-Validation/",
+        github:"github.com/Garvit1809/Form-Validation"
+    },
+    {
+        id:4,
+        name:"Samurai_battle",
+        description: "A Javascript fighting game made using Html-Canvas",
+        tags:["html canvas","css","javascript"],
+
+        demo:"garvit1809.github.io/Samurai-Battle/",
+        github:"github.com/Garvit1809/Samurai-Battle"
+    },
+    {
+        id:5,
+        name:"Task Manager",
+        description: "A simple backend project performing simple CRUD operations",
+        tags:["node", "express", "mongoDb", "axios"],
+        demo:"task-manager-project2.herokuapp.com/",
+        github:"github.com/Garvit1809/Task-Manager"
+    },
+    {
+        id:6,
+        name:"Eco-venture",
+        description: "First hackathon & Won the #BestEnvironmentalHack, its to educate others about environment in a creative & fun way",
+        tags:["python", "nextcord"],
+        demo:"devpost.com/software/eco-friend-draft?ref_content=my-projects-tab&ref_feature=my_projects",
+        github:"github.com/sophiadt/eco-venture"
+    },
+    {
+        id:7,
+        name:"RookieBot",
+        description: "Won the #BestBeginnerHack, a basic utility bot which can manage your server and has features like pomodoro, todolist etc..",
+        tags:["python", "discord.py"],
+        demo:"devpost.com/software/rookiebot",
+        github:"replit.com/@AryanBhasin3/Garvit-test#main.py"
+    },
+    {
+        id:8,
+        name:"Memories",
+        description: "A full stack apllication built using MERN. IN PROGRESS😁😁😁😁",
+        tags:["react", "node", "express", "mongoDb"],
+        demo:"",
+        github:""
+    },
+    {
+        id:9,
+        name:"Weirdoes",
+        description: "A NFT collector UI application built using React, styled components, Framer Motion, React-spring etc. IN PROGRESS😁😁😁😁",
+        tags:["react", "styled-components", "framer-motion", "react-spring"],
+        demo:"",
+        github:""
+    }
+    
+  
+] 
