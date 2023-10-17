996f17dfdde7fd7526a78f98407012e5d86988ce
Garvit1809 <garvitvarshne18@gmail.com>
2023-10-17 14:52:09 +0530

    adding projects

--- src/data/WorkData.js
+++ src/data/WorkData.js
@@ -1,102 +1,86 @@
 export const Work = [
-    {
-        id:1,
-        name:"Taxi Animation",
-        description: "A simple moving car animation using simple css",
-        tags:["html","css"],
+  {
+    id: 1,
+    name: "Taxi Animation",
+    description: "A simple moving car animation using simple css",
+    tags: ["html", "css"],
 
-        demo:"garvit1809.github.io/Taxi-Animation/",
-        github:"github.com/Garvit1809/Taxi-Animation/tree/master"
-    },
-    {
-        id:2,
-        name:"Drum-Kit",
-        description: "A simple drum-kit website built using javascipt dom and event listeners only",
-        tags:["html","css","javascript"],
+    demo: "garvit1809.github.io/Taxi-Animation/",
+    github: "github.com/Garvit1809/Taxi-Animation/tree/master",
+  },
+  {
+    id: 2,
+    name: "Samurai_battle",
+    description: "A Javascript fighting game made using Html-Canvas",
+    tags: ["html canvas", "css", "javascript"],
 
-        demo:"garvit1809.github.io/Drum-Kit",
-        github:"github.com/Garvit1809/Drum-Kit"
-    },
-    {
-        id:3,
-        name:"Form-Validation",
-        description: "Adding form validation on the client side using javascript",
-        tags:["html","css","javascript"],
-
-        demo:"garvit1809.github.io/Form-Validation/",
-        github:"github.com/Garvit1809/Form-Validation"
-    },
-    {
-        id:4,
-        name:"Samurai_battle",
-        description: "A Javascript fighting game made using Html-Canvas",
-        tags:["html canvas","css","javascript"],
-
-        demo:"garvit1809.github.io/Samurai-Battle/",
-        github:"github.com/Garvit1809/Samurai-Battle"
-    },
-    {
-        id:5,
-        name:"Task Manager",
-        description: "A simple backend project performing simple CRUD operations",
-        tags:["node", "express", "mongoDb", "axios"],
-        demo:"task-manager-project2.herokuapp.com/",
-        github:"github.com/Garvit1809/Task-Manager"
-    },
-    {
-        id:6,
-        name:"Eco-venture",
-        description: "First hackathon & Won the #BestEnvironmentalHack, its to educate others about environment in a creative & fun way",
-        tags:["python", "nextcord"],
-        demo:"devpost.com/software/eco-friend-draft?ref_content=my-projects-tab&ref_feature=my_projects",
-        github:"github.com/sophiadt/eco-venture"
-    },
-    {
-        id:7,
-        name:"RookieBot",
-        description: "Won the #BestBeginnerHack, a basic utility bot which can manage your server and has features like pomodoro, todolist etc..",
-        tags:["python", "discord.py"],
-        demo:"devpost.com/software/rookiebot",
-        github:"replit.com/@AryanBhasin3/Garvit-test#main.py"
-    },
-    {
-        id:8,
-        name:"Weirdoes",
-        description: "A NFT collector UI application built using React, styled components, gsap, react-use, reat-confetti etc.",
-        tags:["react", "styled-components", "gsap", "react-confetti"],
-        demo:"prismatic-naiad-f1dac2.netlify.app",
-        github:"github.com/Garvit1809/Weirdoes"
-    },
-    {
-        id: 9,
-        name: 'EcoTogether',
-        description: 'EcoTogether is an application through which people can come together to clean Environment.',
-        tags: ['react','node','express','mongoDb','mongoose','styled-components'],
-        demo: 'devpost.com/software/ecotogether-krcuof',
-        github: 'github.com/Garvit1809/Eco'
-    },
-    {
-        id: 10,
-        name: 'Wibezz',
-        description: `A fashion agency's landing page with smooth animations`,
-        tags: ['react','gsap','framer-motion','locomotive-scroll','styled-components'],
-        demo: 'transcendent-tulumba-5617f9.netlify.app/',
-        github: 'github.com/Garvit1809/Wibe'
-    },
-    {
-        id: 11,
-        name: 'WorkedIn',
-        description: 'COMING SOON',
-        tags: ['react','typescript','node.js','express','mongodb','mongoose'],
-        demo: 'github.com/Garvit1809/WorkedIn',
-        github: 'github.com/Garvit1809/WorkedIn'
-    },
-    {
-        id: 12,
-        name: 'Stresser',
-        description: `Stresser stresses on reducing peoples's stress and anxiety by providing therapy, diet, diary and chat features`,
-        tags: ['react','javascript','node.js','express','mongodb','mongoose'],
-        demo: 'stresser-on.herokuapp.com/',
-        github: 'github.com/Garvit1809/Stresser'
-    }
-] 
+    demo: "garvit1809.github.io/Samurai-Battle/",
+    github: "github.com/Garvit1809/Samurai-Battle",
+  },
+  {
+    id: 3,
+    name: "Stresser",
+    description: `Stresser stresses on reducing peoples's stress and anxiety by providing therapy, diet, diary and chat features`,
+    tags: ["react", "javascript", "node.js", "express", "mongodb", "mongoose"],
+    demo: "github.com/Garvit1809/Stresser",
+    github: "github.com/Garvit1809/Stresser",
+  },
+  {
+    id: 4,
+    name: "Wibezz",
+    description: `A fashion agency's landing page with smooth animations`,
+    tags: [
+      "react",
+      "gsap",
+      "framer-motion",
+      "locomotive-scroll",
+      "styled-components",
+    ],
+    demo: "transcendent-tulumba-5617f9.netlify.app/",
+    github: "github.com/Garvit1809/Wibe",
+  },
+  {
+    id: 5,
+    name: "Weirdoes",
+    description:
+      "A NFT collector UI application built using React, styled components, gsap, react-use, reat-confetti etc.",
+    tags: ["react", "styled-components", "gsap", "react-confetti"],
+    demo: "prismatic-naiad-f1dac2.netlify.app",
+    github: "github.com/Garvit1809/Weirdoes",
+  },
+  {
+    id: 6,
+    name: "Menlo",
+    description: `Built the frontend of our MVP `,
+    tags: ["react", "javascript", "node.js", "express", "mongodb", "mongoose"],
+    demo: "cool-froyo-53e4c9.netlify.app/",
+    github: "cool-froyo-53e4c9.netlify.app/",
+  },
+  {
+    id: 7,
+    name: "WorkedIn",
+    description:
+      "An application through which you can collab with folks with good work ethics",
+    tags: ["react", "typescript", "node.js", "express", "mongodb", "mongoose"],
+    demo: "github.com/Garvit1809/WorkedIn",
+    github: "github.com/Garvit1809/WorkedIn",
+  },
+  {
+    id: 8,
+    name: "Gumroad",
+    description:
+      "A fun project built in 4 days after getting exhausted from exams",
+    tags: ["react", "javascript", "styled-components"],
+    demo: "garvits-gumroad.netlify.app",
+    github: "github.com/Garvit1809/Gumroad",
+  },
+  {
+    id: 9,
+    name: "Teach And Learn",
+    description:
+      "An ed-tech platform where students can teach and learn from each other.",
+    tags: ["react", "typescript", "node.js", "express", "mongodb", "mongoose"],
+    demo: "teachandlearn.app/",
+    github: "github.com/Garvit1809/teachnlearn",
+  },
+];
